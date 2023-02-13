6a653b98ecdb44f5ddaa5d564211f36696b65cdb
Manoel Clemente <37196074+ManoelClemente1@users.noreply.github.com>
2023-02-13 17:17:18 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,38 @@
 import json
 import boto3
 
-
-AWS_REGION = 'us-east-2'
-
-sqs_client = boto3.client("sqs", region_name=AWS_REGION)
-
 def lambda_handler(event, context):
-
-    response = sqs_client.receive_message(
-        QueueUrl="",
-        MaxNumberOfMessages=1,
-        WaitTimeSeconds=10,
-    )
-            
-    print(response)
+    
+    ### vars
+    caminho_do_arquivo_local="/tmp/arquivo.txt"
+    nome_do_arquivo_local="arq_dentro_do_s3.txt"
+    nome_do_s3="bucket_name"
+    
+    ### Cria arquivo
+    arquivo = open(caminho_do_arquivo_local, 'w')
+    arquivo.write('linha 1 \n')
+    arquivo.write('linha 2 \n')
+    arquivo.write('linha 3 \n')
+    arquivo.close()
+    
+    ### Realiza Upload do arquivo
+    s3 = boto3.client('s3')
+    with open(caminho_do_arquivo_local, "rb") as f:
+        s3.upload_fileobj(f, nome_do_s3, nome_do_arquivo_local) 
+    
+    print('Mensagem recebida')
+    print(event)
+    
+    for record in event.get('Records'):
+        print('Mensagem recebida - Records')
+        print(record)
         
-    for message in response.get("Messages", []):
-        message_body = ["Body"]
-        print(f"Message body: {json.loads(message_body)}")
+        body = json.loads(record.get('body'))
+        id_person = body.get('id')
+        name = body.get('Name')
+        
+        print(f'Name: {name}, ID: {id_person}')
+
+        
+        
         
