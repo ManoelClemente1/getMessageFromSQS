67d5a88ed74a4d9f1b44857f15d63845aa8fb70a
Manoel Clemente <37196074+ManoelClemente1@users.noreply.github.com>
2023-02-14 21:13:19 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,38 +1,50 @@
 import json
 import boto3
+from datetime import date
+
 
 def lambda_handler(event, context):
     
-    ### vars
-    caminho_do_arquivo_local="/tmp/arquivo.txt"
-    nome_do_arquivo_local="arq_dentro_do_s3.txt"
-    nome_do_s3="bucket_name"
-    
-    ### Cria arquivo
-    arquivo = open(caminho_do_arquivo_local, 'w')
-    arquivo.write('linha 1 \n')
-    arquivo.write('linha 2 \n')
-    arquivo.write('linha 3 \n')
-    arquivo.close()
-    
-    ### Realiza Upload do arquivo
     s3 = boto3.client('s3')
-    with open(caminho_do_arquivo_local, "rb") as f:
-        s3.upload_fileobj(f, nome_do_s3, nome_do_arquivo_local) 
     
     print('Mensagem recebida')
     print(event)
     
-    for record in event.get('Records'):
-        print('Mensagem recebida - Records')
-        print(record)
-        
-        body = json.loads(record.get('body'))
-        id_person = body.get('id')
-        name = body.get('Name')
-        
-        print(f'Name: {name}, ID: {id_person}')
+    ### Definindo nome do arquivo
+    today = date.today().strftime('%Y%m%d')
+    print(today)
+    
+    ### vars
+    caminho_do_arquivo_local="/tmp/arquivo.txt"
+    nome_do_arquivo_local=today +".txt"
+    nome_do_s3="bucket_name"
+    
+    try:
+    
+        for record in event.get('Records'):
+            print('#### Records ####')
+            print(record)
+            
+            body = record.get('body')
+            
+            print('#### Body ####')
+            print(body)
+            
+            ### Cria arquivo
+            arquivo = open(caminho_do_arquivo_local, 'a')
+            
+            arquivo.write(body +'\n')
+            
+            ### Fecha arquivo
+            arquivo.close()
+            
+            with open(caminho_do_arquivo_local, "rb") as f:
+                s3.upload_fileobj(f, nome_do_s3, nome_do_arquivo_local)
+            
 
+
+    except Exception as e:
+        return e
         
-        
+
         
